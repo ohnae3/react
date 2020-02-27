42fa92ed4d0ffab875fe45b8e9f9817e1ec72120
fixalot <fixalot84@gmail.com>
2020-02-27 17:59:10 +0900

    logback 추가

--- src/main/java/react/serverside/spring/mvc/controller/ExampleController.java
+++ src/main/java/react/serverside/spring/mvc/controller/ExampleController.java
@@ -2,35 +2,42 @@ package react.serverside.spring.mvc.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ExampleController {
-	@RequestMapping(path = "/example/draw-example.do", method = RequestMethod.GET)
+	private static final Logger logger = LoggerFactory.getLogger(ExampleController.class);
+
+	private ExampleController() {}
+
+//	@RequestMapping(path = "/example/draw-example.do", method = RequestMethod.GET)
+	@GetMapping("/example/draw-example.do")
 	public ModelAndView drawExample(ModelAndView mv) {
 		
-		System.out.println("잘 들어 오는군?");
+		logger.debug("잘 들어 오는군?");
 		
 		mv.setViewName("/example/example");
 		return mv;
 	}
 	
-	@RequestMapping(path = "/example/session-invalidate-test.do", method = RequestMethod.GET)
+//	@RequestMapping(path = "/example/session-invalidate-test.do", method = RequestMethod.GET)
+	@GetMapping("/example/session-invalidate-test.do")
 	public ModelAndView sessionInvalidateTest(ModelAndView mv, HttpSession session) {
 		
-		System.out.println("session id: \n" + session.getId());
+		logger.info("session id: \n{}", session.getId());
 		
 		session.setAttribute("a", 123);
 		int a = (int) session.getAttribute("a");
-		System.out.println(String.valueOf(a == 123));
+		logger.info(String.valueOf(a == 123));
 		
 		session.invalidate(); // 세션 무효화
 		
 		// 아이디는 아직 그대로
-		System.out.println("session id after invalidate: \n" + session.getId());
+		logger.info("session id after invalidate: \n{}", session.getId());
 				
 		mv.setViewName("/example/example");
 		return mv;
---
+++ src/main/java/react/serverside/spring/mvc/controller/NameMeController.java
@@ -0,0 +1,14 @@
+package react.serverside.spring.mvc.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class NameMeController {
+	@SuppressWarnings("unused")
+	private static final Logger logger = LoggerFactory.getLogger(NameMeController.class);
+
+	private NameMeController() {}
+
+}
